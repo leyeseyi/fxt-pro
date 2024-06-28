472ce6c3dd7037992f61398ee4a3ce9c56fa38f8
kingsamito <usikpedosamuel@yahoo.com>
2024-06-28 15:27:27 +0100

    major changes

--- src/components/PricingPlan.vue
+++ src/components/PricingPlan.vue
@@ -367,6 +367,7 @@ export default {
         left: 0;
         right: 0;
         bottom: 0;
+        justify-content: center;
         align-items: center;
         height: 100vh;
         border-radius: 0;
